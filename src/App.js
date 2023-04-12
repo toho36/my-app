fee53550ea40fa1403dd4d30604779395f30a3d0
toho36 <toho0036@gmail.com>
2023-04-12 19:34:04 +0200

    First component
    creating first component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
+import ExpenseItem from './commponents/ExpenseItem';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload text.
-        </p>
-        <p>home page</p>
-        <p>new develop text</p>
-        <p>javascriptsss</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Starting</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
