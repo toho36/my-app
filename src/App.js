ea3e59d7c70bed603d97065c9186c9c2eb3491ee
toho36 <toho0036@gmail.com>
2023-04-14 11:43:04 +0200

    Splitting components
    Date to different component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -24,28 +29,28 @@ function App() {
   ];
 
   return (
-    <div className="App">
+    <div>
       <h2>Starting</h2>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
