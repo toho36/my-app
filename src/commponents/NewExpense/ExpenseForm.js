b7f63a4b365df443aef144df7dd8c5531b685fb2
toho36 <toho0036@gmail.com>
2023-04-17 19:43:54 +0200

    57. two-way Binding
    very useful for forms

--- src/commponents/NewExpense/ExpenseForm.js
+++ src/commponents/NewExpense/ExpenseForm.js
@@ -38,13 +38,21 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
     console.log(expenseData);
+
+    setEnteredTitle('');
+    setEnteredDate('');
+    setEnteredAmount('');
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -53,6 +61,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
+            value={enteredAmount}
           />
         </div>
         <div className="new-expense__control">
@@ -62,6 +71,7 @@ const ExpenseForm = () => {
             min="2020-01-01"
             max="2023-12-31"
             onChange={dateChangeHandler}
+            value={enteredDate}
           />
         </div>
       </div>
