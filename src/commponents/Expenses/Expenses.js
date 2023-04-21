7b67458a21f8d7efc2fb901975ee393519d52f54
toho36 <toho0036@gmail.com>
2023-04-21 10:12:25 +0200

    66. Outputting conditional content
    giving message when the filtered year is empty and making JSX lean

--- src/commponents/Expenses/Expenses.js
+++ src/commponents/Expenses/Expenses.js
@@ -13,6 +13,18 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +32,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterDataHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
